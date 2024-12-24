47961cb0097725a91dd5d60b3533f44a8f8d9b91
AbdalahMhmod <Abdullahmahmoud4550@gmail.com>
2024-12-24 12:49:00 +0200

    add test project

---
+++ Nitaj.Tests/ToDoServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using Nitaj.Application.Interfaces;
+
+namespace Nitaj.Tests
+{
+    public class ToDoServiceTests
+    {
+        private readonly IToDoService _toDoService;
+        public ToDoServiceTests(IToDoService toDoService)
+        {
+            _toDoService = toDoService;
+        }
+
+        public async void CompleteToDo_When_Id_1()
+        {
+            // Arrange
+            int id = 1;
+            // Act
+            var result = await _toDoService.CompleteToDo(id);
+            var expectedResult = true;
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+        public async void CompleteToDo_When_Id_0()
+        {
+            // Arrange
+            int id = 0;
+            // Act
+            var result = await _toDoService.CompleteToDo(id);
+            var expectedResult = true;
+            // Assert
+            Assert.That(expectedResult, Is.Not.EqualTo(result));
+        }
+
+    }
+}
